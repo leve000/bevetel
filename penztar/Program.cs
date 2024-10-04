2126791432c8ce166b9f443c6e7a31b422ec28c7
Gyurcsaklj <gyurcsakl@kkszki.hu>
2024-10-04 12:26:28 +0200

    Penztar project is ready to use.

---
+++ penztar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace penztar
+{
+    internal class Program
+    {
+        public static int[] tm= new int[] {500, 1000, 2000, 5000, 10000, 20000 };
+        public static int[] money = new int[6];
+
+    public static void beker()
+    { 
+        for (int i=0; i<tm.Length; i++)
+        {
+            Console.WriteLine($"Kérem {i+1}. bankjegy értékét: ");
+            money[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    public static int bevetel()
+    {
+        int sum = 0;
+
+        for (int i = 0; i < money.Length; i++)
+        {
+            sum = sum + tm[i] * money[i];
+        }
+        return sum;
+    }
+    static void Main(string[] args)
+        {
+        beker();
+        Console.WriteLine($"A napi bevétel {bevetel()} forint volt");
+        }
+    }
+}
